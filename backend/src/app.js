3348decd302802223b3c8c30fae6d5cdb2a8fc63
rushi1302 <109797754+rushi1302@users.noreply.github.com>
2024-02-28 17:23:44 +0530

    starting ecommerse app

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port ", { PORT });
+})
